2a2e2bb5b51ec06f4dd182c1026d5eece65802cf
Armaan48985 <armaanthakur2308@gmail.com>
2023-12-04 20:01:13 +0530

    suspense

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+import DashboardSkeleton from './ui/skeletons'
+
+const loading = () => {
+  return (
+    <div><DashboardSkeleton/></div>
+  )
+}
+
+export default loading
